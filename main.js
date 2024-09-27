1bcaeceebed845798329a4ae05650e30153cf12b
chrisnnaji001 <nnaji8546@gmail.com>
2024-09-27 22:27:57 +0100

    commitdis

---
+++ main.js
@@ -0,0 +1,56 @@
+// String Manipulation Functions
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function countCharacters(str) {
+    return str.length;
+}
+
+function capitalizeWords(sentence) {
+    return sentence.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+
+// Array Functions
+
+function findMax(arr) {
+    return Math.max(...arr);
+}
+
+function findMin(arr) {
+    return Math.min(...arr);
+}
+
+function sumArray(arr) {
+    return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+function filterArray(arr, condition) {
+    return arr.filter(condition);
+}
+
+
+// Mathematical Functions
+
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function fibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib.slice(0, n);
+}
